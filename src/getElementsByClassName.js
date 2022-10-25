497391b74cb02a85ecc71afbf68c059a92e89c1d
Thomas Saldana <saldana.tom@gmail.com>
2022-10-25 09:34:01 -0700

    Pass assertion tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,20 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+var getElementsByClassName = function(className) {
+  var elementsWithClassName = [];
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+  var helperFunction = function (element) {
+    if (element.classList && element.classList.contains(className)) {
+      elementsWithClassName.push(element);
+    }
+
+    if (element.hasChildNodes()) {
+      var childElements = element.childNodes;
+      for (var i = 0; i < childElements.length; i++) {
+        helperFunction(childElements[i]);
+      }
+    }
+
+  };
+  helperFunction(document.body);
+
+  return elementsWithClassName;
 };
